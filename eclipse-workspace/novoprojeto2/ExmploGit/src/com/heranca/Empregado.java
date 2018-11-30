e6164ba8eef73af9c7e90f121966a1206cd387eb
aos12061 <aos12061@gmail.com>
2018-11-30 14:41:29 -0200

    Criação da classe Empregado

---
+++ eclipse-workspace/novoprojeto2/ExmploGit/src/com/heranca/Empregado.java
@@ -0,0 +1,13 @@
+package com.heranca;
+
+public class Empregado {
+
+	protected String nome;
+	protected String telefone;
+	
+	public Empregado(String nome, String telefone) {
+		this.nome = nome;
+		this.telefone = telefone;
+	}
+	
+}
